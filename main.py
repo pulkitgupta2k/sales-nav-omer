41c720a92ddb496e81d6e241c42e7ed447bd29fb
Pulkit Gupta <pulkitgupta2k@gmail.com>
2021-05-12 10:53:30 +0530

    changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import json
 import csv
 
+
 def login(driver):
     link = "https://www.linkedin.com/login"
     driver.get(link)
@@ -19,30 +20,41 @@ def login(driver):
     pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
 
 
-def get_sale(link, driver):
+def get_sale(link, driver, data):
     driver.get(link)
     time.sleep(4)
     html = driver.page_source
+    # with open("test.txt", "w", encoding="utf-8") as f:
+    #     f.write(html)
     soup = BeautifulSoup(html, "html.parser")
     try:
         name = soup.find("div", {"id": "ember57"}).text.strip()
     except:
         name = ""
     try:
-        industry = soup.findAll("div", {"class": "t-14"})[1].text.split("·")[0].strip()
+        industry = soup.findAll(
+            "div", {"class": "t-14"})[1].text.split("·")[0].strip()
     except:
         industry = ""
     try:
-        employees = soup.findAll("div", {"class": "t-14"})[1].text.split("·")[1].strip().split(" ")[0]
+        employees = soup.findAll(
+            "div", {"class": "t-14"})[1].text.split("·")[1].strip().split(" ")[0]
     except:
         employees = ""
     try:
-        dec_makers = soup.find("a", {"data-control-name": "decision_makers_search_link"}).contents[0].strip()
+        location = soup.find(
+            "div", {"class": "t-12 t-black--light"}).text.strip()
+    except:
+        location = ""
+    try:
+        dec_makers = soup.find(
+            "a", {"data-control-name": "decision_makers_search_link"}).contents[0].strip()
         dec_makers = dec_makers.split("(")[1].split(")")[0]
     except:
         dec_makers = ""
     try:
-        website = soup.find("a", {"data-control-name": "visit_company_website"})['href']
+        website = soup.find(
+            "a", {"data-control-name": "visit_company_website"})['href']
     except:
         website = ""
     try:
@@ -51,34 +63,60 @@ def get_sale(link, driver):
     except:
         linkedin_link = ""
     try:
-        growths = [x.text.strip() for x in soup.findAll("dt", {"class": "t-14"})][-3:]
+        growths = [x.text.strip()[:-1]
+                   for x in soup.findAll("dt", {"class": "t-14"})][-3:]
     except:
-        growths = []
-    ret = [name, industry, employees, dec_makers, website, linkedin_link]
+        growths = ["", "", ""]
+
+    if(len(growths) < 3):
+        growths = ["", "", ""]
+
+    if name in data.keys():
+        for i in range(3):
+            if growths[i] > data[name][i]:
+                data[name][i] = growths[i]
+                growths[i] += " ++"
+            elif growths[i] < data[name][i]:
+                data[name][i] = growths[i]
+                growths[i] += " --"
+    else:
+        data[name] = growths
+    ret = [name, industry, employees, location,
+           dec_makers, website, linkedin_link]
     ret.extend(growths)
+    print(name)
     return ret
-    
 
 
 def get_sales(driver):
     with open("urls.txt") as f:
         links = f.readlines()
-    
-    data = [["Name", "Industry", "Employees", "Decision Makers", "Website", "Linkedin Link", "Growth 6m", "Growth 1y", "Growth 2y"]]
+    with open("data.json") as f:
+        d = json.load(f)
+    data = [["Name", "Industry", "Employees", "Location", "Decision Makers",
+             "Website", "Linkedin Link", "Growth 6m", "Growth 1y", "Growth 2y"]]
     for link in links:
         link = link.strip()
-        data.append(get_sale(link, driver))
-
-    with open("result.csv", "w", newline='') as f:
+        try:
+            data.append(get_sale(link, driver, d))
+        except:
+            pass
+    with open("data.json", "w", encoding='utf-8') as f:
+        json.dump(d, f)
+    with open("result.csv", "w", newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerows(data)
 
+
 if __name__ == "__main__":
-    driver = webdriver.Chrome('./chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome('./chromedriver', options=options)
     driver.get("https://www.linkedin.com")
     # login(driver)
     cookies = pickle.load(open("cookies.pkl", "rb"))
-    for cookie in cookies: 
+    for cookie in cookies:
         driver.add_cookie(cookie)
     get_sales(driver)
     driver.close()
